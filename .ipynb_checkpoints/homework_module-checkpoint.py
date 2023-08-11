5f5b3994d5b1e04ff69f1c858bdeeefc69a84668
Kenai Epps <k.epps23@gmail.com>
2023-08-10 20:17:53 -0700

    first commit

---
+++ .ipynb_checkpoints/homework_module-checkpoint.py
@@ -0,0 +1,16 @@
+def sq_foot(length, width):
+    """
+    calculate square footage of house
+    
+    """
+    area = length * width
+    return area    
+
+def circumfrence(radius):
+    """
+    calculate circumfrence of a circle
+
+    """
+    circle_area = 3.14 * radius ** 2
+    return circle_area
+
---
+++ homework_module.py
@@ -0,0 +1,18 @@
+from math import pi, ceil
+
+def sq_foot(length, width):
+    """
+    calculate square footage of house
+
+    """
+    area = length * width
+    return area    
+
+def circumfrence(radius):
+    """
+    calculate circumfrence of a circle
+
+    """
+    circle_area = pi * radius ** 2
+    return circle_area
+
